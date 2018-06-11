24fa1552f95bde2459663048dabf3fa0abfb68bc
Elinor Bissery <eilsib@users.noreply.github.com>
2018-06-11 16:18:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,3 +2,13 @@
 var bot = require('./functions/bot.js');
 
 bot.login();
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
